2cd65fd80f0795026ee9c431940b816077934068
Nino Guinot Toffoli <nguinot-@f4r12s6.clusters.42paris.fr>
2025-09-09 20:06:35 +0200

    norme

--- mainbonus.c
+++ mainbonus.c
@@ -6,7 +6,7 @@
 /*   By: nguinot- <nguinot-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:27:48 by nguinot-          #+#    #+#             */
-/*   Updated: 2025/09/09 10:57:20 by nguinot-         ###   ########.fr       */
+/*   Updated: 2025/09/09 19:42:54 by nguinot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	check_argc(int argc, char **argv)
 	{
 		ft_putstr_fd("usage: ", 2);
 		ft_putstr_fd(argv[0], 2);
-		ft_putstr_fd( "infile \"cmd1\" \"cmd2\" outfile\n", 2);
+		ft_putstr_fd("infile \"cmd1\" \"cmd2\" outfile\n", 2);
 		exit (1);
 	}
 	return (0);
