776da8908ef0ca8eec110bb51ea2aab913d66d46
Nino Guinot Toffoli <nguinot-@f4r13s8.clusters.42paris.fr>
2025-10-18 15:49:04 +0200

    final

--- mainbonus.c
+++ mainbonus.c
@@ -6,30 +6,18 @@
 /*   By: nguinot- <nguinot-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:27:48 by nguinot-          #+#    #+#             */
-/*   Updated: 2025/09/09 20:27:48 by nguinot-         ###   ########.fr       */
+/*   Updated: 2025/10/18 15:48:54 by nguinot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-int	check_argc(int argc, char **argv)
-{
-	if (argc != 5)
-	{
-		ft_putstr_fd("usage: ", 2);
-		ft_putstr_fd(argv[0], 2);
-		ft_putstr_fd(" infile \"cmd1\" \"cmd2\" outfile\n", 2);
-		exit (1);
-	}
-	return (0);
-}
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_exec	ex;
 	int		l;
 
-	check_argc(argc, argv);
+	check_arg_bonus(argc, argv);
 	init_exec(&ex, argc, argv, envp);
 	while_fork(&ex);
 	free(ex.pipes);
